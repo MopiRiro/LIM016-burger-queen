3c6fbf826b791586da2e62087ace76c0bb170b94
Elanny Torres <torres.elanny@gmail.com>
2022-03-08 03:47:38 -0500

    creación dinámica de los productos en place order

--- my-app/src/app/waiterview/place-order/place-order.component.ts
+++ my-app/src/app/waiterview/place-order/place-order.component.ts
@@ -10,15 +10,47 @@ export class PlaceOrderComponent implements OnInit {
   faTrash = faTrashAlt;
 
   clientName:any = "";
+  cant:number = 0;
+  price:number = 0;
+  productList:any = [
+    {
+      item: "Product1",
+      price: 23
+    }, {
+      item: "Product2",
+      price: 20
+    }, {
+      item: "Product3",
+      price: 34
+    }
+  ];
+  subTotal:number = 0;
+  igv:number = this.subTotal*18/100;
+  total:number = this.subTotal + this.igv;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.productList.item)
+  }
+
+  addCnt(){
+    this.cant += 1;
+  }
+
+  subtractCnt(){
+    this.cant -= 1;
   }
 
   sendOrder(){
-    console.log(this.clientName)
+    console.log(this.clientName);
+    console.log(this.cant);
     this.clientName = "";
+    this.cant = 0;
   }
 
+  cancelOrder(){
+    this.clientName = "";
+    this.cant = 0;
+  }
 }
